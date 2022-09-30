47fb44523fa111fa5f686ce68b881f9b8bdab095
Mohit Singh Parihar <mohitsinghparihar1511@gmail.com>
2022-09-30 18:36:26 +0530

    Updates

---
+++ precache-manifest.ad9c2dbcd107e1da5d0d2b3ffbb82bab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa9553717a49671433f461bc5b074a8",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "6413e40778a43f6a794b",
+    "url": "/react-resume/static/css/main.3813d7bd.chunk.css"
+  },
+  {
+    "revision": "80b75557ca5410e6246c",
+    "url": "/react-resume/static/js/2.59249bc1.chunk.js"
+  },
+  {
+    "revision": "55b79ebb5e774a9f9242c9bbc135e8e0",
+    "url": "/react-resume/static/js/2.59249bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6413e40778a43f6a794b",
+    "url": "/react-resume/static/js/main.68756371.chunk.js"
+  },
+  {
+    "revision": "81e33ab131e6ddb44ce2",
+    "url": "/react-resume/static/js/runtime-main.ed2b7e0f.js"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/react-resume/static/media/gitlogo.3e54ed15.png"
+  },
+  {
+    "revision": "a08f59f44e32ab5f5fe579b7b4088b97",
+    "url": "/react-resume/static/media/linkedinLogo.a08f59f4.png"
+  },
+  {
+    "revision": "3cb6597b9a7340684ca0c5bb58625386",
+    "url": "/react-resume/static/media/mailLogo.3cb6597b.png"
+  },
+  {
+    "revision": "6927aab364de747a87f59686e830c4bf",
+    "url": "/react-resume/static/media/phoneLogo.6927aab3.png"
+  }
+]);
